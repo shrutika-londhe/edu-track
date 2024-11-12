ad4720bb968f14e0690feef03ebb237c1fdc8759
ShrutikaLondhe15 <shrutikalondhe15@gmail.com>
2024-11-12 23:53:26 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Header from "./components/layouts/header";
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+};
+
+export default App;
